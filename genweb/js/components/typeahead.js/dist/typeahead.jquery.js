45c17ea1957689d83ef677f1c941379c9f15afe2
Iago López Fernández <iago.lopez@upcnet.es>
2021-03-19 11:25:45 +0100

    Arreglar error resultados búsqueda

--- genweb/js/components/typeahead.js/dist/typeahead.jquery.js
+++ genweb/js/components/typeahead.js/dist/typeahead.jquery.js
@@ -330,7 +330,7 @@
                     o.className && (wrapperNode.className = o.className);
                     patternNode = textNode.splitText(match.index);
                     patternNode.splitText(match[0].length);
-                    //wrapperNode.appendChild(patternNode.cloneNode(true));
+                    wrapperNode.appendChild(patternNode.cloneNode(true));
                     textNode.parentNode.replaceChild(wrapperNode, patternNode);
                 }
                 return !!match;
